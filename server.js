fea0f9321931f8bac8ff5791a2aca10697b8182c
xmthms18 <xmthms@gmail.com>
2023-08-07 15:01:47 -0400

    final commit

---
+++ server.js
@@ -0,0 +1,56 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var axios = require('axios');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); // Change from 'jade' to 'ejs'
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Define a route for /cats/fact
+app.get('/cats/fact', (req, res) => {
+  const apiUrl = 'https://catfact.ninja/fact';
+
+  axios.get(apiUrl)
+    .then(response => {
+      const catFact = response.data.fact;
+      res.render('cat/fact', { catFact });
+    })
+    .catch(error => {
+      res.status(500).send('Error occurred while fetching cat facts.');
+    });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
